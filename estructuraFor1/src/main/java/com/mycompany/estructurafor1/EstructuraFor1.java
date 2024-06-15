4362b2c8e79f8c2adbdda9e3e3e0ebdbc85440ad
Leyder2023 <151747360+Leyder2023@users.noreply.github.com>
2024-06-14 23:57:02 -0500

    Add files via upload

---
+++ estructuraFor1/src/main/java/com/mycompany/estructurafor1/EstructuraFor1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.estructurafor1;
+
+/**
+ *
+ * @author john
+ */
+public class EstructuraFor1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ estructuraFor2/src/main/java/com/mycompany/estructurafor2/EstructuraFor2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.estructurafor2;
+
+/**
+ *
+ * @author john
+ */
+public class EstructuraFor2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
